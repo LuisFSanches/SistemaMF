3978663e593ab65fa1e56f4adf179ec91d94d6b7
LuisF <luis-lipe@hotmail.com>
2024-11-23 20:22:15 -0300

    criação do usuário

--- backend/src/controllers/admin/GetAllAdminController.ts
+++ backend/src/controllers/admin/GetAllAdminController.ts
@@ -1,13 +1,23 @@
-import {Request, Response} from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { GetAllAdminService } from '../../services/admin/GetAllAdminService';
+import { BadRequestException } from '../../exceptions/bad-request';
 
 class GetAllAdminController{
-  async handle(req: Request, res: Response) {
-    const getAllAdminService = new GetAllAdminService();
-    const admins = await getAllAdminService.execute();
+	async handle(req: Request, res: Response, next: NextFunction) {
+		const getAllAdminService = new GetAllAdminService();
+		const admins = await getAllAdminService.execute();
 
-    return res.json(admins);
-  }
+		if ('error' in admins && admins.error) {
+			next(new BadRequestException(
+				admins.message,
+				admins.code
+			));
+
+			return;
+		}
+
+		return res.json(admins);
+	}
 }
 
 export { GetAllAdminController }
--- backend/src/services/admin/GetAllAdminService.ts
+++ backend/src/services/admin/GetAllAdminService.ts
@@ -1,21 +1,26 @@
 import prismaClient from '../../prisma';
 import { ErrorCodes } from "../../exceptions/root";
 
-  class GetAllAdminService{
-    async execute() {
-      try {
-        const admins = await prismaClient.admin.findMany({
-          where: {
-            role: 'ADMIN'
-          }
-        });
+class GetAllAdminService{
+	async execute() {
+		try {
+			const admins = await prismaClient.admin.findMany({
+				where: {
+					role: 'ADMIN'
+				},
+				select: {
+					id: true,
+					name: true,
+					username: true
+				}
+			});
 
-        return { admins };
+			return { admins };
 
-      } catch(error: any) {
-        return { error: true, message: error.message, code: ErrorCodes.SYSTEM_ERROR }
-      }
-    }
-  }
-  
-  export { GetAllAdminService }
+			} catch(error: any) {
+				return { error: true, message: error.message, code: ErrorCodes.SYSTEM_ERROR }
+			}
+		}
+	}
+
+export { GetAllAdminService }
