5dfa016906b452e6f51fab22a99b69f8e72546fe
LuisF <luis-lipe@hotmail.com>
2024-12-07 14:11:54 -0300

    remover as aspas duplas da variável

--- backend/src/utils/getCertificates.ts
+++ backend/src/utils/getCertificates.ts
@@ -3,11 +3,13 @@ import fs from 'fs';
 
 export const getCertificates = () => {
 
-    console.log('certificado', process.env.BANCO_INTER_API_CERT_PATH)
+    const cert = process.env.BANCO_INTER_API_CERT_PATH!.replace(/(^"|"$)/g, '');
+    const key = process.env.BANCO_INTER_API_KEY_PATH!.replace(/(^"|"$)/g, '');
+
     const httpsAgent = new https.Agent({
         host: 'cdpj.partners.bancointer.com.br',
-        cert: Buffer.from(process.env.BANCO_INTER_API_CERT_PATH!, 'utf-8'),
-        key: Buffer.from(process.env.BANCO_INTER_API_KEY_PATH!, 'utf-8'),
+        cert: Buffer.from(cert, 'utf-8'),
+        key: Buffer.from(key, 'utf-8'),
         rejectUnauthorized: true
     })
     
