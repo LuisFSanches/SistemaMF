4caa7aad7e504607ab1e050f99fe6ee756224f3f
LuisF <luis-lipe@hotmail.com>
2024-12-09 21:59:51 -0300

    alterada url da api do inter

--- backend/src/services/pix/GetPixService.ts
+++ backend/src/services/pix/GetPixService.ts
@@ -5,11 +5,12 @@ class GetPixService{
     async execute(token: string, httpsAgent: any, initial_date: any, final_date: any, limit: any) {
         const params = new URLSearchParams({
             dataInicio: initial_date,
-            dataFim: final_date
+            dataFim: final_date,
+            tipoTransacao: "PIX"
         });
 
         try {
-            const url = `https://cdpj.partners.bancointer.com.br/banking/v2/extrato?${params.toString()}`;
+            const url = `https://cdpj.partners.bancointer.com.br/banking/v2/extrato/completo?${params.toString()}`;
 
             const response = await axios.get(url, {
                 headers: {
@@ -25,17 +26,11 @@ class GetPixService{
                 throw new Error("O campo 'transactions' não é um array.");
             }
 
-            const sortedTransactions = transactions.sort((a: any, b: any) => {
-                const dateA = new Date(a.dataEntrada);
-                const dateB = new Date(b.dataEntrada);
-                return dateB.getTime() - dateA.getTime();
-            });
-
             if (limit) {
-                return sortedTransactions.slice(0, limit);
+                return transactions.slice(0, limit);
             }
 
-            return sortedTransactions;
+            return transactions;
         } catch(error: any) {
             console.log('DEU ERRRO', error);
         }
