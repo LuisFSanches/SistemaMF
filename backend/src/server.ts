ba4ea4c88f9a207b9328958dff87879ca30ec0cc
LuisF <luis-lipe@hotmail.com>
2024-12-21 09:06:53 -0300

    adicionar novo console.log

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -32,6 +32,11 @@ io.on("connection", (socket) => {
   });
 });
 
+app.use((req, res, next) => {
+  console.log(`Recebida requisição: ${req.method} ${req.url}`);
+  next();
+});
+
 app.use(express.json());
 app.use(cors());
 app.use(router);
