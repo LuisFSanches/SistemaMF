e5b2d74cb090a2b2140821aa0bd089542972f60e
LuisF <luis-lipe@hotmail.com>
2024-12-21 09:56:55 -0300

    ajustar o server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
+import http from 'http';
 import 'express-async-errors';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { Server } from 'socket.io';
-import { createServer } from 'http';
-import { eventEmitter } from './controllers/inter/WebhookPixController';
-// import { orderEmitter } from './controllers/order/UpdateOrderController';
+
 import { getCertificatesForWebhook } from './utils/getCertificates';
 
 dotenv.config();
@@ -15,45 +13,14 @@ import { errorMiddleware } from './middlewares/errors';
 
 const app = express();
 const httpsOptions = getCertificatesForWebhook() as any;
-const httpServer = createServer(httpsOptions, app);
-const io = new Server(httpServer, {
-  cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-  },
-  transports: ["websocket"],
-});
-
-io.on("connection", (socket) => {
-  console.log("Cliente conectado ao WebSocket!");
-
-  socket.on("disconnect", () => {
-    console.log("Cliente desconectado do WebSocket.");
-  });
-});
-
-app.use((req, res, next) => {
-  console.log(`Recebida requisição: ${req.method} ${req.url}`);
-  next();
-});
 
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
-eventEmitter.on("pixReceived", (data) => {
-  console.log("Pix Recebido");
-  io.emit("pixPaymentNotification", data); // Envia o evento para todos os clientes conectados
-});
-
-/*orderEmitter.on("orderUpdated", (data) => {
-  console.log("Evento recebido: orderUpdated");
-  io.emit("orderUpdateNotification", data); // Envia o evento para todos os clientes conectados
-});*/
-
 app.use(errorMiddleware);
 
-// Substituir app.listen por httpServer.listen
-httpServer.listen(3333, () => {
-  console.log('Servidor rodando na porta 3333!!!!');
+const PORT = 3333;
+http.createServer(httpsOptions, app).listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
