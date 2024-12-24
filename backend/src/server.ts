d929b8c5f51c144fd81488e18571dee3158f60e2
LuisF <luis-lipe@hotmail.com>
2024-12-23 21:53:46 -0300

    removido novamente o https

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import http from 'http';
-import https from 'https';
 import 'express-async-errors';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -22,12 +21,6 @@ app.use(router);
 app.use(errorMiddleware);
 
 const PORT = 3333;
-https.createServer(httpsOptions, app).listen(PORT, () => {
+http.createServer(httpsOptions, app).listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
-
-if (process.env.IS_PRODUCTION === 'false') {
-  http.createServer(app).listen(3334, () => {
-    console.log(`Servidor rodando em http://localhost:3334`);
-  });
-}
