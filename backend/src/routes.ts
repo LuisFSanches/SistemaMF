3cb2f50988af7f2f12a63c700e226e7be93f68c6
LuisF <luis-lipe@hotmail.com>
2025-02-01 21:25:15 -0300

    add middleware to statistic

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -70,10 +70,8 @@ router.post('/webhook/pix', new WebhookPixController().handle)
 // router.put('/create/webhook', new RegisterWebhookController().handle)
 
 //-- ROTAS STATISTICS --
-router.get('/statistics/top-clients', new TopClientsController().handle);
-router.get('/statistics/daily-sales', new DailySalesController().handle);
-router.get('/statistics/top-admins', new TopAdminsController().handle);
-router.get('/statistics/top-teste', new TopAdminsController().handle);
-
+router.get('/statistics/top-clients', superAdminAuthMiddleware, new TopClientsController().handle);
+router.get('/statistics/daily-sales', superAdminAuthMiddleware, new DailySalesController().handle);
+router.get('/statistics/top-admins', superAdminAuthMiddleware, new TopAdminsController().handle);
 
 export { router };
