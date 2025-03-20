37ce7dc9d805bef6e62b05f6965d2d3047e3edfc
LuisF <luis-lipe@hotmail.com>
2025-03-20 20:59:03 -0300

    adicionar header

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -5,6 +5,6 @@ const baseUrl = process.env.REACT_APP_API_URL || "http://localhost:3334";
 export const api = axios.create({
   baseURL: baseUrl,
   headers: {
-    'X-Custom-Secret': "only-mirai-users",
+    'x-custom-secret': "only-mirai-users",
   },
 });
