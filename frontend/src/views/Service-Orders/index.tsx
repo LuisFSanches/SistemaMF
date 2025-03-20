c8b684434f2a5b2ed3f3a2123d6cc5062f6ccc1f
LuisF <luis-lipe@hotmail.com>
2025-03-20 00:14:31 -0300

    edit service workers page

--- frontend/src/views/Service-Orders/index.tsx
+++ frontend/src/views/Service-Orders/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { IOrder } from "../../interfaces/IOrder";
-import { Container } from "./style";
+import { Container, Orders } from "./style";
 import { updateStatus } from "../../services/orderService";
 import { OrderCard } from "../../components/OrderCard";
 import { useOrders } from "../../contexts/OrdersContext";
@@ -10,14 +10,13 @@ import { EditOrderModal } from "../../components/EditOrderModal";
 export function ServiceOrdersPage(){
 	const { onGoingOrders, editOrder, loadOnGoingOrders } = useOrders();
 
-	const [openedOrders, setOpenedOrders] = useState<IOrder[]>([]);
-	const [inProgressOrders, setInProgressOrders] = useState<IOrder[]>([]);
-	const [inDeliveryOrders, setInDeliveryOrders] = useState<IOrder[]>([]);
+	const [orders, setOrders] = useState<IOrder[]>(onGoingOrders);
 	const [showLoader, setShowLoader] = useState(false);
 	const [requestOrders, setRequestOrders] = useState(false);
     const [editOrderModal, setEditOrderModal] = useState(false);
     const [currentOrder, setCurrentOrder] = useState<IOrder | null>(null);
-
+	const [selectedOrderType, setSelectedOrderType] = useState<string>("all-orders");
+	const [selectedOrderStatus, setSelectedOrderStatus] = useState<string>("all-status");
 
     function handleOpenEditOrderModal(order: IOrder){
         setEditOrderModal(true);
@@ -26,9 +25,6 @@ export function ServiceOrdersPage(){
 
 	const fetchOrders = async () => {
 		setShowLoader(true);
-		setOpenedOrders(onGoingOrders.filter((order: IOrder) => order.status === "OPENED"));
-		setInProgressOrders(onGoingOrders.filter((order: IOrder) => order.status === "IN_PROGRESS"));
-		setInDeliveryOrders(onGoingOrders.filter((order: IOrder) => order.status === "IN_DELIVERY"));
 		setShowLoader(false);
 	};
 
@@ -38,6 +34,7 @@ export function ServiceOrdersPage(){
 			loadOnGoingOrders();
 			setRequestOrders(true);
 		}
+		setOrders(onGoingOrders);
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [onGoingOrders]);
 
@@ -56,8 +53,25 @@ export function ServiceOrdersPage(){
 								margin: 0;
 								padding: 20px;
 
-								.order-actions, .edit-icon, .order-type, .message-content {
+								.order-actions, .edit-icon, .order-type, .message-content, button {
 									display: none;
+								}
+
+								p, h3 {
+									margin: 10px 0;
+									font-size: 16px;
+								}
+
+								strong {
+									font-size: 16px;
+								}
+
+								h3 {
+									font-weight: 500;
+								}
+
+								.expanded-content {
+									display: block !important;
 								}
 							}
 						</style>
@@ -80,74 +94,128 @@ export function ServiceOrdersPage(){
 		setShowLoader(false);
 	}
 
+	const getNextOrderStatus = (status: string) => {
+		switch (status) {
+			case "OPENED":
+				return {
+					status: "IN_PROGRESS",
+					action: "Produção"
+				};
+			case "IN_PROGRESS":
+				return {
+					status: "IN_DELIVERY",
+					action: "Entrega"
+				};
+			case "IN_DELIVERY":
+				return {
+					status: "DELIVERED",
+					action: "Finalizado"
+				};
+			default:
+				return {
+					status: "OPENED",
+					action: "Ordem Aberta"
+				};
+		}
+	};
+
+	const getPreviousOrderStatus = (status: string) => {
+		switch (status) {
+			case "IN_DELIVERY":
+				return {
+					status: "IN_PROGRESS",
+					action: "Produção"
+				};
+			case "IN_PROGRESS":
+				return {
+					status: "OPENED",
+					action: "Ordem Aberta"
+				};
+			default:
+				return {
+					status: "OPENED",
+					action: "Ordem Aberta"
+				};
+		}
+	};
+
+	const filterOrders = (type: string, status: string) => {
+		const filtered = onGoingOrders.filter((order: IOrder) => {
+			const matchesType =
+			type === "all-orders"
+				? true
+				: type === "counter-orders"
+				? !order.online_order
+				: order.online_order;
+		
+			const matchesStatus = status === "all-status" ? true : order.status === status;
+		
+			return matchesType && matchesStatus;
+		});
+		
+		setOrders(filtered);
+	};
+
+	const filterOrdersByType = (type: string) => {
+		setSelectedOrderType(type);
+		filterOrders(type, selectedOrderStatus);
+	};
+
+	const filterByOrderStatus = (status: string) => {
+		setSelectedOrderStatus(status);
+		filterOrders(selectedOrderType, status);
+	};
     return (
 		<Container>
-			<Loader show={showLoader} />
-			<div className="order-container opened">
-				<header className="opened-order">
-					Ordem Aberta
-				</header>
-				{openedOrders.map((order: any) => (
-					<div key={order.id} id={`order-card-${order.id}`}>
-						<OrderCard order={order}
-							handlePrint={handlePrint}
-							handleOrderStatus={handleOrderStatus}
-							buttonStatus="to-production"
-							previousButtonStatus={null}
-							nextStatus="IN_PROGRESS"
-							previousStatus={null}
-							nextAction="Em produção"
-							previousAction={null}
-							handleOpenEditOrderModal={handleOpenEditOrderModal}
-						/>
-					</div>
-				))}
-			</div>
-			<div className="order-container in_progress">
-				<header className="in-progress-order">
-					Em produção
-				</header>
-				{inProgressOrders.map((order: any) => (
-          			<div key={order.id} id={`order-card-${order.id}`}>
-						<OrderCard order={order}
-							handlePrint={handlePrint}
-							handleOrderStatus={handleOrderStatus}
-							buttonStatus="to-finished"
-							previousButtonStatus="to-production"
-							nextStatus="IN_DELIVERY"
-							previousStatus="OPENED"
-							nextAction="Entrega"
-							previousAction="Em produção"
-							handleOpenEditOrderModal={handleOpenEditOrderModal}
-						/>
-            		</div>
-            	))}
-			</div>
-			<div className="order-container in_delivery">
-				<header className="finished-order">
-					Rota de Entrega
-				</header>
-				{inDeliveryOrders.map((order: any) => (
-          			<div key={order.id} id={`order-card-${order.id}`}>
-						<OrderCard order={order}
-							handlePrint={handlePrint}
-							handleOrderStatus={handleOrderStatus}
-							buttonStatus="delivered"
-							previousButtonStatus="to-finished"
-							nextStatus="DONE"
-							previousStatus="IN_PROGRESS"
-							nextAction="Finalizado"
-							previousAction="Entrega"
-							handleOpenEditOrderModal={handleOpenEditOrderModal}
-						/>
-          			</div>
-          		))}
-			</div>
-			<EditOrderModal
-                isOpen={editOrderModal}
-                onRequestClose={() => setEditOrderModal(false)}
-                order={currentOrder as any}
-            />
+			<header>
+				<button className={`all-orders ${selectedOrderType === "all-orders" ? "active" : ""}`}
+					onClick={() => filterOrdersByType("all-orders")}>
+						Todos
+				</button>
+				<button className={`counter-orders ${selectedOrderType === "counter-orders" ? "active" : ""}`}
+					onClick={() => filterOrdersByType("counter-orders")}>
+						Ordens Balcão
+					</button>
+				<button className={`online-orders ${selectedOrderType === "online-orders" ? "active" : ""}`}
+					onClick={() => filterOrdersByType("online-orders")}>
+						Ordens Online
+				</button>
+				<select onChange={(e) => filterByOrderStatus(e.target.value)}>
+					<option value={"all-status"}>Todos</option>
+					<option value={"OPENED"}>Abertas</option>
+					<option value={"IN_PROGRESS"}>Em produção</option>
+					<option value={"IN_DELIVERY"}>Em entrega</option>
+				</select>
+			</header>
+			<Orders>
+				<Loader show={showLoader} />
+				<div className="order-container">
+					{orders.map((order: any) => {
+						const { status: nextStatus, action: nextAction } = getNextOrderStatus(order.status);
+						const { status: previousStatus, action: previousAction } = getPreviousOrderStatus(order.status);
+						return (
+							<div key={order.id} id={`order-card-${order.id}`}>
+								<OrderCard order={order}
+									handlePrint={handlePrint}
+									handleOrderStatus={handleOrderStatus}
+									buttonStatus="to-production"
+									previousButtonStatus={null}
+									nextStatus={nextStatus}
+									previousStatus={previousStatus}
+									nextAction={nextAction}
+									previousAction={previousAction}
+									handleOpenEditOrderModal={handleOpenEditOrderModal}
+								/>
+							</div>
+						)
+					})}
+				</div>
+				<EditOrderModal
+					isOpen={editOrderModal}
+					onRequestClose={() => setEditOrderModal(false)}
+					order={currentOrder as any}
+				/>
+			</Orders>
 		</Container>
 	)
 }
