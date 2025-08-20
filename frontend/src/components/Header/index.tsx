1d7bbe64f6f84d78dd392a2ad6891d5f12ce7508
LuisF <luis-lipe@hotmail.com>
2025-08-19 21:33:48 -0300

    create option to generate card

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { Container, MenuButton } from "./style";
 import { GlobalMenu } from '../GlobalMenu';
+import { GenerateCard } from "../GenerateCard";
 import logoFull from '../../assets/images/logo.png'
 
 export function Header() {
@@ -11,11 +12,14 @@ export function Header() {
     return (
         <Container>
             <img src={logoFull} alt="logo" title="logo"/>
-            <MenuButton type="button" onClick={() => setMenuModal(true)}>
-                <FontAwesomeIcon icon={faBars} />
-                <span>Menu</span>
-            </MenuButton>
-
+            <div>
+                <GenerateCard />
+                <MenuButton type="button" onClick={() => setMenuModal(true)}>
+                    <FontAwesomeIcon icon={faBars} />
+                    <span>Menu</span>
+                </MenuButton>
+            </div>
+            
             <GlobalMenu isOpen={menuModal} onRequestClose={() => setMenuModal(false)}/>
         </Container>
     )
